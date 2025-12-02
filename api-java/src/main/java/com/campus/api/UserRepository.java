fcef5a851eb9e0dd585140e298fdfeeb7850ebba
Changwan <wjsckddhks99@gmail.com>
2025-12-02 22:09:17 +0900

    Revert "feat: 이메일 기반 회원가입·로그인을 위한 AuthController 및 UserRepository 메서드 추가"
    This reverts commit 1640b4f06d489581b18c62260ab4990a163eeecb.

--- api-java/src/main/java/com/campus/api/UserRepository.java
+++ api-java/src/main/java/com/campus/api/UserRepository.java
@@ -2,13 +2,6 @@ package com.campus.api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    // 이메일 중복 체크
     boolean existsByEmail(String email);
-
-    // 로그인용으로 이메일로 유저 찾기
-    Optional<User> findByEmail(String email);
 }
